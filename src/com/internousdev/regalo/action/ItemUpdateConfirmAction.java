11db0a012c8be78d86e07e89989d01bef3e13346
shokichi627 <shokichi627@gmail.com>
2018-06-04 17:45:22 +0900

    ファイル作成

---
+++ src/com/internousdev/regalo/action/ItemUpdateConfirmAction.java
@@ -0,0 +1,19 @@
+package com.internousdev.regalo.action;
+
+import java.sql.SQLException;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ItemUpdateConfirmAction extends ActionSupport implements SessionAware{
+
+	public String execute() throws SQLException{
+
+		String result = SUCCESS;
+
+		return result;
+
+	}
+
+}
