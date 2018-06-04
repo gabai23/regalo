65e9a3f1444d93ad4d028ba49cb256de6ad18ab5
rt-1217 <alt_fluff@yahoo.co.jp>
2018-06-04 17:47:27 +0900

    SQL SearchAction

---
+++ sql/regalo.sql
@@ -0,0 +1,62 @@
+set names utf8
+set foreign_key_checks=0;
+
+drop database if exists Regalo;
+create database if not exists Regalo;
+
+use Regalo;
+
+create table user_info(
+id int primary key not null auto_increment comment "ID",
+user_id varchar(16) unique not null comment "ユーザーID",
+password varchar(16) not null comment "パスワード",
+family_name varchar(32) not null comment "姓",
+first_name varchar(32) not null comment "名",
+family_name_kana varchar(32) not null comment "姓かな",
+first_name_kana varchar(32) not null comment "名かな",
+sex tinyint not null default 0 comment "性別",
+email varchar(32) not null comment "メールアドレス",
+status tinyint not null default 0 comment "ステータス",
+logined tinyint not null default 0 comment "ログインフラグ",
+regist_date datetime not null comment "登録日",
+update_date datetime not null comment "更新日"
+)
+
+default charset=utf8
+comment="会員情報テーブル";
+
+insert into user_info values
+(1,"admin","admin","管理","管理","かんり","かんり",0,"guest@gmail.com",0,0,now(),now()
+(2,"guest","guest","ゲスト","ゲスト","げすと","げすと",0,"guest@gmail.com",0,0,now(),now()
+);
+
+create table product_info(
+id int primary key not null auto_increment comment "ID",
+product_id int unique not null comment "商品ID",
+product_name varchar(100) unique not null comment "商品名",
+product_name_kana varchar(100) not null comment "商品名かな",
+product_description varchar(255) not null comment"商品詳細",
+category_id int not null not null comment "カテゴリID",
+price int comment "価格",
+image_file_path varchar(100) comment "画像ファイルパス",
+image_file_name varchar(50) comment "画像ファイル名",
+release_date datetime not null comment "発売年月",
+release_company varchar(50) comment "ステータス",
+regist_date datetime not null comment "登録日",
+update_date datetime comment "更新日",
+foreign key(category_id) references m_category(category_id)
+)
+default charset=utf8
+comment="商品情報テーブル";
+
+insert into product_info values
+(1,1,"ギフト","ぎふと","ギフトの商品詳細",1,1000,"画像パス","画像ファイル名",now(),"発売会社",0,now(),now());
+
+
+
+
+
+
+
+
+
