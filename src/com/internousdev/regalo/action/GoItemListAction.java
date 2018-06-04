f99b66fad1898d872ce8043a121d721f58116555
snakadate427 <snakadate427@gmail.com>
2018-06-04 17:39:09 +0900

    struts.xml追加

---
+++ src/com/internousdev/regalo/action/GoItemListAction.java
@@ -0,0 +1,14 @@
+package com.internousdev.regalo.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class GoItemListAction extends ActionSupport{
+
+	public String execute() {
+
+		String result = SUCCESS;
+
+		return result;
+	}
+
+}
