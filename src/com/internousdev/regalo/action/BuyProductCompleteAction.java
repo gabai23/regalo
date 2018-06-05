8587df5deefd19317198cb90a59e5006771c6353
internousdev <internousdev@192.168.1.86>
2018-06-05 18:08:50 +0900

    郡山

---
+++ src/com/internousdev/regalo/action/BuyProductCompleteAction.java
@@ -0,0 +1,11 @@
+package com.internousdev.regalo.action;
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionSupport;
+public class BuyProductCompleteAction extends ActionSupport {
+	public Map<String,Object>session;
+public String execute() {
+	return SUCCESS;
+}
+}
+
