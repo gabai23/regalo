b6d51ea63118d5dadae46f0b6e005b76365f7e86
shokichi627 <shokichi627@gmail.com>
2018-06-05 11:14:08 +0900

    ItemDeleteComplete→ItemDeleteCompleteAction

--- src/com/internousdev/regalo/action/ItemDeleteCompleteAction.java
+++ src/com/internousdev/regalo/action/ItemDeleteCompleteAction.java
@@ -7,7 +7,7 @@ import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class ItemDeleteComplete extends ActionSupport implements SessionAware{
+public class ItemDeleteCompleteAction extends ActionSupport implements SessionAware{
 
 	public String execute() throws SQLException{
 
