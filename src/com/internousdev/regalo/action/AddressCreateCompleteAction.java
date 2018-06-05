8a58ebcc0dacab757edea38eb01d3cbc9d62bfbd
internousdev <internousdev@192.168.1.97>
2018-06-05 09:58:17 +0900

    AdressCreateAction

---
+++ src/com/internousdev/regalo/action/AddressCreateCompleteAction.java
@@ -0,0 +1,10 @@
+package com.internousdev.regalo.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class AddressCreateCompleteAction extends ActionSupport {
+	public String execute() {
+		return SUCCESS;
+	}
+
+}
