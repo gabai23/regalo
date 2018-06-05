e1231a3892d731fa205a08831264ea5179de3789
snakadate427 <snakadate427@gmail.com>
2018-06-05 14:39:42 +0900

    仲館 StartActionでゲストユーザーID生成

--- src/com/internousdev/regalo/action/StartAction.java
+++ src/com/internousdev/regalo/action/StartAction.java
@@ -1,14 +1,49 @@
 package com.internousdev.regalo.action;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class StartAction extends ActionSupport{
+//一番初めに読み込むアクション
+public class StartAction extends ActionSupport implements SessionAware{
+
+	private Map<String,Object> session;
 
 	public String execute() {
 
 		String result = SUCCESS;
 
+		RandomStringUtils random = new RandomStringUtils();
+
+		try {
+			if(!(session.containsKey("tempUserId"))) {
+				//ログインフラグ生成
+				session.put("loginFlg", false);
+
+				//ゲストユーザー用IDを生成
+				@SuppressWarnings("static-access")
+				String tempUserId = random.randomAlphabetic(10);
+				session.put("tempUserId", tempUserId);
+
+				System.out.println("StartAction-----");
+				System.out.println("tempUserId:" + tempUserId);
+				System.out.println("-----StartAction");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
 		return result;
 	}
+
+	@Override
+	public void setSession(Map<String, Object> arg0) {
+
+	}
+
+
 
 }
