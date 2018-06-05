d3e18465a590f07ed3d419a8e594e87d633485a7
shokichi627 <shokichi627@gmail.com>
2018-06-05 15:56:19 +0900

    CartのDAOとDTO

--- src/com/internousdev/regalo/action/GoCartAction.java
+++ src/com/internousdev/regalo/action/GoCartAction.java
@@ -1,71 +1,74 @@
 package com.internousdev.regalo.action;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.regalo.dao.CartDAO;
+import com.internousdev.regalo.dto.CartDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class GoCartAction extends ActionSupport implements SessionAware{
 
-//	private String categoryId;
-//	private String keywords;
-//	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
-//	private Map<String, Object> session;
+	private String categoryId;
+	private String keywords;
+	private List<MCategoryDTO> mCategoryDtoList = new ArrayList<MCategoryDTO>();
+	private Map<String, Object> session;
 
 	public String execute() throws SQLException{
 
 		return SUCCESS;
 
-//		String result = ERROR;
-//		String userId = null;
-//		CartInfoDAO cartInfoDao = new CartInfoDAO();
-//		List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();
-//		if(session.containsKey("loginId")) {
-//			userId = String.valueOf(session.get("loginId"));
-//		}else if (session.containsKey("tempUserId")) {
-//			userId = String.valueOf(session.get("tempUserId"));
-//		}
-//		cartInfoDtoList = cartInfoDao.getCartInfoDtoList(userId);
-//		Iterator<CartInfoDTO> iterator = cartInfoDtoList.iterator();
-//		if(!(iterator.hasNext())) {
-//			cartInfoDtoList = null;
-//		}
-//		session.put("cartInfoDtoList", cartInfoDtoList);
-//
-//		int totalPrice = Integer.parseInt(String.valueOf(cartInfoDao.getTotalPrice(userId)));
-//		session.put("totalPrice", totalPrice);
-//		result = SUCCESS;
-//
-//		if(!session.containsKey("mCategoryList")) {
-//			MCategoryDAO mCategoryDao = new MCategoryDAO();
-//			mCategoryDtoList = mCategoryDao.getMCategoryList();
-//			session.put("mCategoryDtoList", mCategoryDtoList);
-//		}
-//		return result;
+		String result = ERROR;
+		String userId = null;
+		CartDAO CartDao = new CartDAO();
+		List<CartDTO> CartDtoList = new ArrayList<CartDTO>();
+		if(session.containsKey("loginId")) {
+			userId = String.valueOf(session.get("loginId"));
+		}else if (session.containsKey("tempUserId")) {
+			userId = String.valueOf(session.get("tempUserId"));
+		}
+		CartDtoList = CartDao.getCartDtoList(userId);
+		Iterator<CartDTO> iterator = CartDtoList.iterator();
+		if(!(iterator.hasNext())) {
+			CartDtoList = null;
+		}
+		session.put("CartDtoList", CartDtoList);
+
+		int totalPrice = Integer.parseInt(String.valueOf(CartDao.getTotalPrice(userId)));
+		session.put("totalPrice", totalPrice);
+		result = SUCCESS;
+
+		if(!session.containsKey("mCategoryList")) {
+			MCategoryDAO mCategoryDao = new MCategoryDAO();
+			mCategoryDtoList = mCategoryDao.getMCategoryList();
+			session.put("mCategoryDtoList", mCategoryDtoList);
+		}
+		return result;
 
 	}
 
-//	public String getCategoryId() {
-//		return categoryId;
-//	}
-//
-//	public void setCategoryId(String categoryId) {
-//		this.categoryId = categoryId;
-//	}
-//
-//	public String getKeywords() {
-//		return keywords;
-//	}
-//
-//	public void setKeywords(String keywords) {
-//		this.keywords = keywords;
-//	}
-//
-//	public Map<String, Object> getSession() {
-//		return session;
-//	}
+	public String getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public Map<String, Object> getSession() {
+		return session;
+	}
 
 	@Override
 	public void setSession(Map<String, Object> arg0) {
