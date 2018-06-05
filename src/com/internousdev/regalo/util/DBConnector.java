5ef2aa8528ef6d511d795bb956e82882d4369449
snakadate427 <snakadate427@gmail.com>
2018-06-05 10:16:49 +0900

    DBConnector

--- src/com/internousdev/regalo/util/DBConnector.java
+++ src/com/internousdev/regalo/util/DBConnector.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnector {
 
 	private static String driverName = "com.mysql.jdbc.Driver";
-	private static String url = "jdbc:mysql://localhost/regalo";
+	private static String url = "jdbc:mysql://localhost/Regalo";
 
 	private static String user = "root";
 	private static String password = "mysql";
