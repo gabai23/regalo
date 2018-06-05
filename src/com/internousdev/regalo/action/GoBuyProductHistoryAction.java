c2d05a75e7067737e7efc87ca535d1c98d9bc011
makikouzaki0628 <kusnapiy971@email.com>
2018-06-05 17:19:14 +0900

    MyPage Action/DAO/DTO 鴻崎

--- src/com/internousdev/regalo/action/GoBuyProductHistoryAction.java
+++ src/com/internousdev/regalo/action/GoBuyProductHistoryAction.java
@@ -2,7 +2,7 @@ package com.internousdev.regalo.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class GoBuyItemHistoryAction extends ActionSupport{
+public class GoBuyProductHistoryAction extends ActionSupport{
 	public String execute() {
 		return SUCCESS;
 	}
