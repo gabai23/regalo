e504743ff93ec764628946b23996088183241816
internous <internous.dev@gmail.com>
2018-06-05 11:20:22 +0900

    GoAddressCreateAction

--- src/com/internousdev/regalo/action/GoAddressCreateAction.java
+++ src/com/internousdev/regalo/action/GoAddressCreateAction.java
@@ -2,7 +2,7 @@ package com.internousdev.regalo.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class GoAddressCreateAtion extends ActionSupport {
+public class GoAddressCreateAction extends ActionSupport {
 	public String execute() {
 		return SUCCESS;
 	}
