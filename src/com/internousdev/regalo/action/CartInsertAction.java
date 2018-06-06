dc87ca31a155bdf4d66b3a9d07b196d927c2ca4b
shokichi627 <shokichi627@gmail.com>
2018-06-06 11:42:54 +0900

    cart.jspとCartInsertAction.javaの作成

--- src/com/internousdev/regalo/action/CartInsertAction.java
+++ src/com/internousdev/regalo/action/CartInsertAction.java
@@ -1,139 +1,139 @@
 package com.internousdev.regalo.action;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.regalo.dao.CartDAO;
+import com.internousdev.regalo.dto.CartDTO;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class CartInsertAction extends ActionSupport implements SessionAware{
 
-//	private int productId;
-//	private String productName;
-//	private String productNameKana;
-//	private String imageFilePath;
-//	private String imageFileName;
-//	private int price;
-//	private String productCount;
-//	private String releaseCompany;
-//	private Date releaseDate;
-//	private String productDescription;
-//
-//	private String categoryId;
-//
-//	private Map<String, Object> session;
+	private int productId;
+	private String productName;
+	private String productNameKana;
+	private String imageFilePath;
+	private String imageFileName;
+	private int price;
+	private String productCount;
+	private String releaseCompany;
+	private Date releaseDate;
+	private String productDescription;
+
+	private String categoryId;
+
+	private Map<String, Object> session;
 
 	public String execute() {
 
-		String result = SUCCESS;
 
+		String result=ERROR;
+		String userId = null;
+		String tempUserId = null;
+
+		if(session.containsKey("loginId")) {
+			userId = String.valueOf(session.get("loginId"));
+		}
+		if (!(session.containsKey("loginId")) && session.containsKey("tempUserId")) {
+			userId = String.valueOf(session.get("tempUserId"));
+			tempUserId = String.valueOf(session.get("tempUserId"));
+		}
+		productCount = String.valueOf((productCount.split(" ,",0))[0]);
+
+		CartDAO CartDao = new CartDAO();
+		int count = CartDao.regist(userId,tempUserId,productId,productCount,price);
+		if(count > 0) {
+			result=SUCCESS;
+		}
+		List<CartDTO> CartDtoList = new ArrayList<CartDTO>();
+		CartDtoList = CartDao.getCartDtoList(userId);
+		Iterator<CartDTO> iterator = CartDtoList.iterator();
+		if(!(iterator.hasNext())) {
+			CartDtoList = null;
+		}
+		session.put("CartDtoList", CartDtoList);
+		int totalPrice = Integer.parseInt(String.valueOf(CartDao.getTotalPrice(userId)));
+		session.put("totalPrice", totalPrice);
 		return result;
-
-//		String result=ERROR;
-//		String userId = null;
-//		String tempUserId = null;
-//		 if (!(session.containsKey("loginId")) && !(session.containsKey("tempUserId"))) {
-//			 CommonUtility commonUtility = new CommonUtility();
-//			 session.put("tempUserId", commonUtility.getRamdomValue());
-//		}
-//		if(session.containsKey("loginId")) {
-//			userId = String.valueOf(session.get("loginId"));
-//		}
-//		if (!(session.containsKey("loginId")) && session.containsKey("tempUserId")) {
-//			userId = String.valueOf(session.get("tempUserId"));
-//			tempUserId = String.valueOf(session.get("tempUserId"));
-//		}
-//		productCount = String.valueOf((productCount.split(" ,",0))[0]);
-//
-//		CartInfoDAO cartInfoDao = new CartInfoDAO();
-//		int count = cartInfoDao.regist(userId,tempUserId,productId,productCount,price);
-//		if(count > 0) {
-//			result=SUCCESS;
-//		}
-//		List<CartInfoDTO> cartInfoDtoList = new ArrayList<CartInfoDTO>();
-//		cartInfoDtoList = cartInfoDao.getCartInfoDtoList(userId);
-//		Iterator<CartInfoDTO> iterator = cartInfoDtoList.iterator();
-//		if(!(iterator.hasNext())) {
-//			cartInfoDtoList = null;
-//		}
-//		session.put("cartInfoDtoList", cartInfoDtoList);
-//		int totalPrice = Integer.parseInt(String.valueOf(cartInfoDao.getTotalPrice(userId)));
-//		session.put("totalPrice", totalPrice);
-//		return result;
 
 	}
 
-//	public int getProductId() {
-//		return productId;
-//	}
-//	public void setProductId(int productId) {
-//		this.productId = productId;
-//	}
-//	public String getProductName() {
-//		return productName;
-//	}
-//	public void setProductName(String productName) {
-//		this.productName = productName;
-//	}
-//	public String getProductNameKana() {
-//		return productNameKana;
-//	}
-//	public void setProductNameKana(String productNameKana) {
-//		this.productNameKana = productNameKana;
-//	}
-//	public String getImageFilePath() {
-//		return imageFilePath;
-//	}
-//	public void setImageFilePath(String imageFilePath) {
-//		this.imageFilePath = imageFilePath;
-//	}
-//	public String getImageFileName() {
-//		return imageFileName;
-//	}
-//	public void setImageFileName(String imageFileName) {
-//		this.imageFileName = imageFileName;
-//	}
-//	public int getPrice() {
-//		return price;
-//	}
-//	public void setPrice(int price) {
-//		this.price = price;
-//	}
-//
-//	public String getProductCount() {
-//		return productCount;
-//	}
-//	public void setProductCount(String productCount) {
-//		this.productCount = productCount;
-//	}
-//	public String getReleaseCompany() {
-//		return releaseCompany;
-//	}
-//	public void setReleaseCompany(String releaseCompany) {
-//		this.releaseCompany = releaseCompany;
-//	}
-//	public Date getReleaseDate() {
-//		return releaseDate;
-//	}
-//	public void setReleaseDate(Date releaseDate) {
-//		this.releaseDate = releaseDate;
-//	}
-//	public String getProductDescription() {
-//		return productDescription;
-//	}
-//	public void setProductDescription(String productDescription) {
-//		this.productDescription = productDescription;
-//	}
-//
-//	public String getCategoryId() {
-//		return categoryId;
-//	}
-//	public void setCategoryId(String categoryId) {
-//		this.categoryId = categoryId;
-//	}
-//	public Map<String, Object> getSession() {
-//		return session;
-//	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getProductNameKana() {
+		return productNameKana;
+	}
+	public void setProductNameKana(String productNameKana) {
+		this.productNameKana = productNameKana;
+	}
+	public String getImageFilePath() {
+		return imageFilePath;
+	}
+	public void setImageFilePath(String imageFilePath) {
+		this.imageFilePath = imageFilePath;
+	}
+	public String getImageFileName() {
+		return imageFileName;
+	}
+	public void setImageFileName(String imageFileName) {
+		this.imageFileName = imageFileName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getProductCount() {
+		return productCount;
+	}
+	public void setProductCount(String productCount) {
+		this.productCount = productCount;
+	}
+	public String getReleaseCompany() {
+		return releaseCompany;
+	}
+	public void setReleaseCompany(String releaseCompany) {
+		this.releaseCompany = releaseCompany;
+	}
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+
+	public String getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(String categoryId) {
+		this.categoryId = categoryId;
+	}
+	public Map<String, Object> getSession() {
+		return session;
+	}
 
 	@Override
 	public void setSession(Map<String, Object> arg0) {
