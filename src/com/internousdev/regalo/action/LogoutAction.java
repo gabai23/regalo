f0903e477c56ce2fbe246122d29d715c556fa44f
internousdev <internousdev@192.168.1.100>
2018-06-07 11:50:59 +0900

    圭　LogOutAction

--- src/com/internousdev/regalo/action/LogoutAction.java
+++ src/com/internousdev/regalo/action/LogoutAction.java
@@ -1,10 +1,31 @@
 package com.internousdev.regalo.action;
 
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
 import com.opensymphony.xwork2.ActionSupport;
 
-public class LogoutAction extends ActionSupport{
+public class LogoutAction extends ActionSupport implements SessionAware{
+	
+	private Map<String,Object> session;
+
+	
 
 	public String execute(){
+		
+		session.remove("userId");
+		session.put("loginFlg", false);
+		
+		
 		return SUCCESS;
+	}
+	
+	public Map<String, Object> getSession() {
+		return session;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.session = session;
 	}
 }
