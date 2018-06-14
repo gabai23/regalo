fdf026ad57a43c022ada0b36998fadb812c51dd8
snakadate427 <snakadate427@gmail.com>
2018-06-14 15:49:02 +0900

    仲館 ProductInsertCompleteAction DAO 修正

--- src/com/internousdev/regalo/dao/ProductInsertCompleteDAO.java
+++ src/com/internousdev/regalo/dao/ProductInsertCompleteDAO.java
@@ -11,11 +11,13 @@ public class ProductInsertCompleteDAO {
 	private DateUtil dateUtil = new DateUtil();
 	private String sql = "INSERT INTO product_info(product_id,product_name,product_name_kana,price,product_stock,image_file_path,image_file_name,release_company,product_description,category_id,release_date,regist_date)VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 
-	public void buyProductInfo(int productId, String productName, String productNameKana, String productDescription,
+	public int buyProductInfo(int productId, String productName, String productNameKana, String productDescription,
 			int categoryId, int price, String imageFilePath, String imageFileName) throws SQLException {
 
 		DBConnector dbConnector = new DBConnector();
 		 Connection connection = dbConnector.getConnection();
+
+		 int count = 0;
 
 		try {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -33,12 +35,14 @@ public class ProductInsertCompleteDAO {
 			preparedStatement.setInt(10,categoryId);
 			preparedStatement.setString(11,dateUtil.getDate());
 			preparedStatement.setString(12, dateUtil.getDate());
-			preparedStatement.execute();
+			count = preparedStatement.executeUpdate();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}finally {
 			connection.close();
 		}
+
+		return count;
 	}
 
 
